e1f5d474e90d6f701045640d74d57a5b0a766f91
Adam Joyce <adamjoyce@outlook.com>
2016-04-14 18:03:51 +0100

    Building scalable mesh

--- Assets/Scripts/SurfaceGenerator.cs
+++ Assets/Scripts/SurfaceGenerator.cs
@@ -3,18 +3,36 @@
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class SurfaceGenerator : MonoBehaviour {
 
+    [Range(1, 200)]
+    public int resolution = 10;
+
     private Mesh mesh;
+    private int currentResolution;
 
     // Refresh the surface mesh.
     public void Refresh() {
-        mesh.vertices = new Vector3[] {
-            new Vector3(0f, 0f),
-            new Vector3(1f, 0f),
-            new Vector3(0f, 1f)
-        };
+        if (currentResolution != resolution)
+            CreateMeshGrid();
+    }
 
+    // Creates a mesh grid where the size is dependent on the resolution.
+    private void CreateMeshGrid() {
+        currentResolution = resolution;
+        int gridLength = currentResolution + 1;
+        Vector3[] vertices = new Vector3[gridLength * gridLength];
+        float quadSize = 1f / currentResolution;
+
+        // Build the mesh grid array.
+        for (int i = 0, column = 0; column <= currentResolution; column++) {
+            for (int row = 0; row <= currentResolution; row++, i++) {
+                vertices[i] = new Vector3(row * quadSize - 0.5f, column * quadSize - 0.5f);
+            }
+        }
+
+        mesh.vertices = vertices;
         mesh.triangles = new int[] {
-            0, 1, 2
+            0, 2, 1,
+            1, 2, 3
         };
     }
 
