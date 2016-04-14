686c791cd198b5c4d6cd69d92d2730b455550d9d
Adam Joyce <adamjoyce@outlook.com>
2016-04-14 17:22:08 +0100

    First commit

---
+++ Assets/Editor/SurfaceGeneratorEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SurfaceGenerator))]
+public class SurfaceGeneratorEditor : Editor {
+
+    private SurfaceGenerator generator;
+
+    // Updates changes made in the inspector immediately.
+    public override void OnInspectorGUI() {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if (EditorGUI.EndChangeCheck())
+            RefreshGenerator();
+    }
+
+    // Refreshes the surface generator.
+    private void RefreshGenerator() {
+        if (Application.isPlaying)
+            generator.Refresh();
+    }
+
+    // Ensure the target being inspeccted is the generator and allow for undo and redo refreshes.
+    private void OnEnable() {
+        generator = target as SurfaceGenerator;
+        Undo.undoRedoPerformed += RefreshGenerator;
+    }
+
+    // Allow for undo and redo refreshes.
+    private void OnDisable() {
+        Undo.undoRedoPerformed -= RefreshGenerator;
+    }
+}
---
+++ Assets/Scripts/SurfaceGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SurfaceGenerator : MonoBehaviour {
+
+    private Mesh mesh;
+
+    // Refresh the surface mesh.
+    public void Refresh() {
+        mesh.vertices = new Vector3[] {
+            new Vector3(0f, 0f),
+            new Vector3(1f, 0f),
+            new Vector3(0f, 1f)
+        };
+
+        mesh.triangles = new int[] {
+            0, 1, 2
+        };
+    }
+
+    // Creates a mesh if one does not exist.
+    private void OnEnable() {
+        if (mesh == null) {
+            mesh = new Mesh();
+            mesh.name = "Surface Mesh";
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+
+        Refresh();
+    }
+
+}
