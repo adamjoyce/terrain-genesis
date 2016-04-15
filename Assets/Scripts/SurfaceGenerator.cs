0ab36dbf1d91fb6caca8698d5905e70baa06c1a8
Adam Joyce <adamjoyce@outlook.com>
2016-04-15 15:51:43 +0100

    Basic Value and Perlin noise applied to the mesh

--- Assets/Scripts/SurfaceGenerator.cs
+++ Assets/Scripts/SurfaceGenerator.cs
@@ -22,12 +22,38 @@ public class SurfaceGenerator : MonoBehaviour {
     private Mesh mesh;
     private int currentResolution;
 
+    private Vector3[] vertices;
+    private Vector3[] normals;
+    private Color[] colours;
+
     // Refresh the surface mesh.
     public void Refresh() {
         if (currentResolution != resolution)
             CreateMeshGrid();
 
-        //Vector3 point00 = transform.TransformPoint(new Vector3())
+        // Quad corners.
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
+        Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
+        Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
+
+        NoiseMethod method = Noise.methods[(int)type][dimensions - 1];
+        float quadSize = 1f / currentResolution;
+
+        for (int i = 0, y = 0; y <= resolution; y++) {
+            Vector3 point0 = Vector3.Lerp(point00, point01, y * quadSize);
+            Vector3 point1 = Vector3.Lerp(point10, point11, y * quadSize);
+            for (int x = 0; x <= resolution; x++, i++) {
+                Vector3 point = Vector3.Lerp(point0, point1, x * quadSize);
+                float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+
+                if (type != NoiseMethodType.Value) {
+                    sample = sample * 0.5f + 0.5f;
+                }
+                colours[i] = coloring.Evaluate(sample);
+            }
+        }
+        mesh.colors = colours;
     }
 
     // Creates a mesh grid where the size is dependent on the resolution.
@@ -38,10 +64,11 @@ public class SurfaceGenerator : MonoBehaviour {
         int gridLength = currentResolution + 1;
         float quadSize = 1f / currentResolution;
 
-        Vector3[] vertices = new Vector3[gridLength * gridLength];
+        vertices = new Vector3[gridLength * gridLength];
+        normals = new Vector3[vertices.Length];
+        colours = new Color[vertices.Length];
+
         Vector2[] uvs = new Vector2[vertices.Length];
-        Vector3[] normals = new Vector3[vertices.Length];
-        Color[] colours = new Color[vertices.Length];
 
         // Build the mesh grid vertex array.
         for (int i = 0, y = 0; y <= currentResolution; y++) {
